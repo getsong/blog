3ea11967b560c53d085f3fc29925eed58626638e
personal <getsong13@gmail.com>
2019-04-13 21:55:15 +0800

    post /bloggers

--- app.js
+++ app.js
@@ -8,13 +8,29 @@ app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/blog', {useNewUrlParser: true})
 
 const Blogger = mongoose.model('Blogger', { userName: String });
-const Zildjian = new Blogger({ userName: 'Zildjian' });
-Zildjian.save().then(() => console.log('meow'));
 
 app.get('/', (req, res) => res.send('Hello World from the blog system!'))
 
 app.post('/bloggers', (req, res) => {
-    
+    const blogger = new Blogger({ userName: req.body.userName });
+
+    blogger.save()
+        .then(() => res.send('blogger saved'))
+        .catch(error => {
+            console.log(error);
+            res.send('not saved');
+        });
+})
+
+app.get('/bloggers', (req, res) => {
+    const blogger = new Blogger({ userName: req.body.userName });
+
+    blogger.save()
+        .then(() => res.send('blogger saved'))
+        .catch(error => {
+            console.log(error);
+            res.send('not saved');
+        });
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
