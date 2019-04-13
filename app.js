1c546f12479f75af6877561f4ca9ea1dfbecd011
personal <getsong13@gmail.com>
2019-04-13 21:41:39 +0800

    can save blog from express to mongodb (not handling request yet)

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express()
 const port = 3456
+app.use(bodyParser.json());
+mongoose.connect('mongodb://localhost:27017/blog', {useNewUrlParser: true})
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const Blogger = mongoose.model('Blogger', { userName: String });
+const Zildjian = new Blogger({ userName: 'Zildjian' });
+Zildjian.save().then(() => console.log('meow'));
+
+app.get('/', (req, res) => res.send('Hello World from the blog system!'))
+
+app.post('/bloggers', (req, res) => {
+    
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
